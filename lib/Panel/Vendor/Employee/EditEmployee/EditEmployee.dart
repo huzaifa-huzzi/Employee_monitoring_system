359f024d11f5e235cd96b20d600bec41bc1a4ead
huzaifa-huzzi <huzaifaatta8152@gmail.com>
2026-08-06 12:24:53 +0500

    Vendor Snapshot is done totally

--- lib/Panel/Vendor/Employee/EditEmployee/EditEmployee.dart
+++ lib/Panel/Vendor/Employee/EditEmployee/EditEmployee.dart
@@ -1,15 +1,12 @@
 import 'package:employee_monitoring_system/Panel/Vendor/Employee/EditEmployee/Widget/EditEmployeeWidget.dart';
-import 'package:employee_monitoring_system/Panel/Vendor/Employee/EmployeeController.dart';
 import 'package:employee_monitoring_system/Resources/Colors.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class EditEmployee extends StatelessWidget {
   const EditEmployee({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.find<EmployeeController>();
 
     return const Scaffold(
       backgroundColor: AppColors.backgroundOfScreenColor,
